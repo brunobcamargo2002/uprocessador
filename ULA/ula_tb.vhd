8dc96380cd4743091421eddb09f14155e6a30ee9
Thomas <thomaspinton@alunos.utfpr.edu.br>
2024-04-13 14:06:37 -0300

    ula (not working)

--- ULA/ula_tb.vhd
+++ ULA/ula_tb.vhd
@@ -2,11 +2,40 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity porta_tb is
-    compoent adder
+entity ula_tb is
+end entity;
+
+architecture a_ula_tb of ula_tb is
+    component ula
         port (
+            operation : in unsigned(1 downto 0);
             in_a : in unsigned(15 downto 0);
             in_b : in unsigned(15 downto 0);
-            result : out unsigned(15 downto 0)
+            saida : out unsigned(15 downto 0);
+            is_zero: out std_logic
         );
-end entity;
+    end component;
+
+    signal operation : unsigned(1 downto 0);
+    signal in_a, in_b, saida : unsigned(15 downto 0);
+    signal is_zero : std_logic;
+    
+begin 
+    uut: ula port map (
+        operation => operation,
+        in_a => in_a,
+        in_b => in_b,
+        saida => saida,
+        is_zero => is_zero
+    );
+    process
+    begin
+        in_a <= "1010101010101010";
+        in_b <= "0101010101010101";
+        operation <= "00";
+        wait for 50 ns;
+        operation <= "01";
+        wait;
+    end process;
+end architecture;
+
