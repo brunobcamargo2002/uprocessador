f764864ea823f3f3438575423972e8155cb0f0e2
Thomas <thomaspinton@alunos.utfpr.edu.br>
2024-04-19 16:26:49 -0300

    register inputs and outputs

--- Register_Bank/reg_bd.vhd
+++ Register_Bank/reg_bd.vhd
@@ -4,7 +4,14 @@ use ieee.numeric_std.all;
 
 entity reg_bd is
     port(
-        read_r1: unsigned()
-
+        read_r1: in unsigned();
+        read_r2: in unsigned();
+        write_enable: in std_logic;
+        write_register: in unsigned(3 downto 0);
+        write_data: in unsigned(15 downto 0);
+        clk: in std_logic;
+        rst: in std_logic;
+        read_data0: out unsigned(15 downto 0); 
+        read_data1: out unsigned(15 downto 0); 
     );
 end entity;
